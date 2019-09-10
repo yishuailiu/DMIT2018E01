91d5af3639812cce174bdeb0f3e1b56d706fbab0
Yishuai Liu <lys940320@hotmail.com>
2019-09-09 20:48:39 -0600

    change

---
+++ WebApp/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ChinookSystem.Data.Entities;
+using System.Data.Entity;
+
+namespace ChinookSystem.DAL
+{
+    internal class ChinookContext:DbContext
+    {
+        public ChinookContext() : base("ChinookDB")
+        {
+            //connection 
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+    }
+}
