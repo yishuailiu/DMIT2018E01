fa17413b7bdc267a0a41295477b508afdcad36be
Yishuai Liu <lys940320@hotmail.com>
2019-09-11 19:39:06 -0600

    update

---
+++ WebApp/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ChinookSystem.DAL;
+using ChinookSystem.Data.Entities;
+
+namespace ChinookSystem.BLL
+{
+    public class AlbumController
+    {
+        public List<Album> Album_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.ToList();
+            }
+        }
+
+        public Album Album_Get(int Albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.Find(Albumid);
+            }
+        }
+    }
+}
---
+++ WebApp/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ChinookSystem.DAL;
+using ChinookSystem.Data.Entities;
+
+namespace ChinookSystem.BLL
+{
+    public class ArtistController
+    {
+        public List<Artist> Artist_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.ToList();
+            }
+        }
+
+        public Artist Artist_Get(int artistid)
+        {
+            using(var context = new ChinookContext())
+            {
+                return context.Artists.Find(artistid);
+            }
+        }
+    }
+}
