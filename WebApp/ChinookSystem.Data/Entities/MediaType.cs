2b4e57efe4d654ed19b9d392b4bcf99002ce8f7f
Yishuai Liu <lys940320@hotmail.com>
2019-09-11 18:56:58 -0600

    class update

---
+++ WebApp/ChinookSystem.Data/Entities/MediaType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("MediaTypes")]
+    public class MediaType
+    {
+        [Key]
+        public int MediaTypeId { get; set; }
+
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Name = null;
+                }
+                else
+                {
+                    _Name = value;
+                }//eof
+            }
+        }//eop
+
+        public virtual Track Tracks { get; set; }
+
+    }
+}
---
+++ WebApp/ChinookSystem.Data/Entities/Track.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Tracks")]
+    public class Track
+    {
+        [Key]
+        public int TrackId { get; set; }
+        public string Name { get; set; }
+        public int? AlbumId { get; set; }
+        public int MediaTypeId { get; set; }
+        public int GenreId { get; set; }
+        private string _Composer;
+        public string Composer
+        {
+            get
+            {
+                return _Composer;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Composer = null;
+                }
+                else
+                {
+                    _Composer = value;
+                }//eof
+            }
+        }//eop
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public double UnitPrice { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
+        public virtual ICollection<MediaType> MediaTypes { get; set; }
+        public virtual ICollection<Genre> Genres { get; set; }
+    }
+}
