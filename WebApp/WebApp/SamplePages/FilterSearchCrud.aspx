12e384a7cf169eb7e784292306f1a1a900b40fdd
Yishuai Liu <lys940320@hotmail.com>
2019-09-13 19:01:17 -0600

    update

---
+++ WebApp/WebApp/SamplePages/FilterSearchCrud.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FilterSearchCrud.aspx.cs" Inherits="WebApp.SamplePages.FilterSearch" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>ReviewCrud</h1>
+    <div class="row">
+        <div class="col-sm-offset-3">
+            <asp:Label Text="Select an artist to view album" ID="label1" runat="server" />&nbsp;
+            <asp:DropDownList runat="server" ID="ArtistList">                
+            </asp:DropDownList>
+        </div>
+    </div>
+</asp:Content>
---
+++ WebApp/WebApp/SamplePages/FilterSearchCrud.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using ChinookSystem.BLL;
+using ChinookSystem.Data.Entities;
+
+namespace WebApp.SamplePages
+{
+    public partial class FilterSearch : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindArtistList();
+            }
+        }
+
+        protected void BindArtistList()
+        {
+            ArtistController sysmgr = new ArtistController();
+
+            List<Artist> info = sysmgr.Artist_List();
+            info.Sort((x, y) => x.Name.CompareTo(y.Name));
+            ArtistList.DataSource = info;
+            ArtistList.DataTextField = nameof(Artist.Name);
+            ArtistList.DataTextField = nameof(Artist.ArtistId);
+
+            ArtistList.DataBind();
+            ArtistList.Items.Insert(0, "select ...");
+        }
+    }
+}
