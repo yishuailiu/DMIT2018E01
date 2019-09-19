72e716c3f0ccda9023d58973c6e303faa187ab83
Yishuai Liu <lys940320@hotmail.com>
2019-09-18 19:18:16 -0600

    Update AlbumController.cs

--- WebApp/ChinookSystem/BLL/AlbumController.cs
+++ WebApp/ChinookSystem/BLL/AlbumController.cs
@@ -14,6 +14,7 @@ namespace ChinookSystem.BLL
     [DataObject]
     public class AlbumController
     {
+        #region query
         public List<Album> Album_List()
         {
             using (var context = new ChinookContext())
@@ -40,6 +41,43 @@ namespace ChinookSystem.BLL
                 return results.ToList();
             }
         }
+        #endregion
 
+        #region Add,Update,Delete
+        public int Album_Add(Album item)
+        {
+            using(var context = new ChinookContext())
+            {
+                context.Albums.Add(item);
+                context.SaveChanges();
+                return item.AlbumId;
+            }
+        }
+        public int Album_Update(Album item)
+        {
+            using(var context = new ChinookContext())
+            {
+                context.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                return context.SaveChanges();
+            }
+        }
+        public int Album_Delete(int albumid)
+        {
+            using(var context = new ChinookContext())
+            {
+                var existing = context.Albums.Find(albumid);
+                if (existing ==null)
+                {
+                    throw new Exception("Album not find");
+                }
+                else
+                {
+                    context.Albums.Remove(existing);
+                    return context.SaveChanges();
+                }
+            }
+        }
+
+        #endregion 
     }
 }
